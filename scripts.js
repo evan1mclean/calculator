1f8346cabba1855559b0bc21bea1102b8ed10d2e
Evan McLean <evanhughmclean@gmail.com>
2022-05-04 12:14:04 -0400

    Operator function and html skeleton
    Added the basic container divs for the calculator and all of the
    buttons. Added the very beginnings of styling it.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+function operate(operator, num1, num2) {
+    let result;
+    switch (operator) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+        case "divide":
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
