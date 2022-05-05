482a8397b01d696395fc9541ffa4890832236ec4
Evan McLean <evanhughmclean@gmail.com>
2022-05-05 11:56:16 -0400

    Adds display screen functions
    Adds a function specifically to listen for button clicks on the number
    and decimal buttons. Adds a function to return a value depending on
    what button is clicked. Adds a function to display said value to the
    display screen.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+//Global variable to store current number clicked
+let currentNumber;
+
+//Next four functions do basic math and return values
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,6 +18,7 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
+//Determines which operator is used and calls the appropriate math function
 function operate(operator, num1, num2) {
     let result;
     switch (operator) {
@@ -32,3 +37,42 @@ function operate(operator, num1, num2) {
     }
     return result;
 }
+
+function setButtonListeners() {
+    let numbers = document.querySelectorAll('.number');
+    numbers.forEach(button => {
+        button.addEventListener('click', function(e) {
+            getNumberFromButton(e);
+            displayNumber(currentNumber);
+        });
+    })
+}
+
+//gets number from button clicked and returns the value
+function getNumberFromButton(e) {
+    currentNumber = e.target.textContent;
+    //checks for if decimal point was clicked
+    if (isNaN(currentNumber)) {
+        return currentNumber = ".";
+    }
+    return currentNumber;
+}
+
+//displays number on the screen
+function displayNumber(number) {
+    display = document.querySelector('.currentOperation');
+    //checks to make sure there's only 1 decimal point in the number
+    if (display.textContent.includes('.') && number === ".") {
+        return;
+    }
+    //beyond 14 digits the screen starts to get stretched out
+    else if (display.textContent.length === 14) {
+        alert("You have reached the calculator's display limit!");
+        return;
+    }
+    else {
+        display.textContent += number;
+    }
+}
+
+setButtonListeners();
